45e41cbda03bb5e1df336d11fbc6324cba827d91
kothale19 <kothale.pujaprasad@gmail.com>
2022-09-20 10:29:14 +0530

    Added: to get number 1 to 6 logic

--- src/com/blz/java/SnakeLadder.java
+++ src/com/blz/java/SnakeLadder.java
@@ -8,10 +8,17 @@ public class SnakeLadder {
 		System.out.println("Welcome to Snake & Ladder Computation Program!!");
 		int position = 0;
 		int player = 1;
+		int count = 0;
 		System.out.println("Start Position=" + position);
 		Random r = new Random();
-		int dice = r.nextInt(6)+1;
+		int dice = r.nextInt(6) + 1;
 		System.out.println("Single player at start position is " + dice);
+		System.out.println("Dice outcome is =" + dice);
+
+		count++;
+
+		System.out.println("Total count " + count);
+
 	}
 
 }
