4f684f65502e3216b51cfd14cb65836f92a21e47
kothale19 <kothale.pujaprasad@gmail.com>
2022-09-20 09:48:47 +0530

    Added: Welcome To Snake & Ladder Program

---
+++ Readme.md
@@ -0,0 +1,1 @@
+This is the Snake & Ladder Computation Program!!
---
+++ src/com/blz/java/SnakeLadder.java
@@ -0,0 +1,10 @@
+package com.blz.java;
+
+public class SnakeLadder {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Snake & Ladder Computation Program!!");
+
+	}
+
+}
