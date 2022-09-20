35f1cf2b999fa891a58711c778f56432a5e8a95c
kothale19 <kothale.pujaprasad@gmail.com>
2022-09-20 10:21:14 +0530

    Added: single player play the game at start position 0 logic

--- src/com/blz/java/SnakeLadder.java
+++ src/com/blz/java/SnakeLadder.java
@@ -1,10 +1,17 @@
 package com.blz.java;
 
+import java.util.Random;
+
 public class SnakeLadder {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Snake & Ladder Computation Program!!");
-
+		int position = 0;
+		int player = 1;
+		System.out.println("Start Position=" + position);
+		Random r = new Random();
+		int dice = r.nextInt(6)+1;
+		System.out.println("Single player at start position is " + dice);
 	}
 
 }
